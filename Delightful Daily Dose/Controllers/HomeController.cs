4c2fe60b0f9168ef1aac6b29d1dedfb0a8b2f4d4
jjanos139 <jdale139@gmail.com>
2022-05-24 15:00:46 +0200

    Add weather, nameday, forex apis, add menu items

--- Delightful Daily Dose/Controllers/HomeController.cs
+++ Delightful Daily Dose/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Delightful_Daily_Dose.Controllers
@@ -18,12 +19,49 @@ namespace Delightful_Daily_Dose.Controllers
             _logger = logger;
         }
 
+        public async Task<string> GetApi(string apiUrl)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+
+                    return data;
+                }
+            }
+            return null;
+        }
+        public async Task<string> GetNameDay()
+        {
+            string apiUrl = $"https://api.nevnapok.eu/ma";
+            return await GetApi(apiUrl);
+        }
+
+        public async Task<string> GetWeather()
+        {
+            string apiUrl =
+                "https://api.openweathermap.org/data/2.5/weather?q=Budapest,hu&units=metric&appid=104d168e3001f454894090545b535f79";
+            return await GetApi(apiUrl);
+        }
+        public async Task<string> GetExchangeRate(string from )
+        {
+            string apiUrl =
+                $"https://api.apilayer.com/exchangerates_data/convert?to=HUF&from={from}&amount=1&apikey=JS7qpwEXHPVbtntwU7H69R1jHnAHj7AA";
+            return await GetApi(apiUrl);
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Credits()
         {
             return View();
         }
--- Delightful Daily Dose/wwwroot/js/site.js
+++ Delightful Daily Dose/wwwroot/js/site.js
@@ -1,4 +1,95 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function loadNameDay() {
+    window.fetch(`Home/GetNameDay`).then((response) => {
+        response.json().then((data) => {
+            let nameDay = document.getElementById("nameday");
+            nameDay.innerHTML = data[Object.keys(data)[0]][0] + ", " + data[Object.keys(data)[0]][1];
+        });
+    });
+}
 
-// Write your JavaScript code.
+function loadWeather() {
+    window.fetch(`Home/GetWeather`).then((response) => {
+        response.json().then((data) => {
+            let weather = document.getElementById("weather");
+            weather.innerHTML += data["main"]["temp"].toFixed(0) + "&#8451;";
+            let weatherIcon = document.getElementById("weather-icon");
+            // broken clouds
+            if (data["weather"][0]["icon"] === "04d")
+                weatherIcon.src = `http://openweathermap.org/img/wn/04d.png`;
+            // clear sky
+            if (data["weather"][0]["icon"] === "01d")
+                weatherIcon.src = `http://openweathermap.org/img/wn/01d.png`;
+            // few clouds
+            if (data["weather"][0]["icon"] === "02d")
+                weatherIcon.src = `http://openweathermap.org/img/wn/02d.png`;
+            // scattered clouds
+            if (data["weather"][0]["icon"] === "03d")
+                weatherIcon.src = `http://openweathermap.org/img/wn/03d.png`;
+            // shower rain
+            if (data["weather"][0]["icon"] === "09d")
+                weatherIcon.src = `http://openweathermap.org/img/wn/09d.png`;
+            // rain
+            if (data["weather"][0]["icon"] === "10d")
+                weatherIcon.src = `http://openweathermap.org/img/wn/10d.png`;
+            // thunderstorm
+            if (data["weather"][0]["icon"] === "11d")
+                weatherIcon.src = `http://openweathermap.org/img/wn/11d.png`;
+            // snow
+            if (data["weather"][0]["icon"] === "13d")
+                weatherIcon.src = `http://openweathermap.org/img/wn/13d.png`;
+            // mist
+            if (data["weather"][0]["icon"] === "50d")
+                weatherIcon.src = `http://openweathermap.org/img/wn/50d.png`;
+        });
+    });
+}
+
+function loadExchangeRate(from) {
+    window.fetch(`Home/GetExchangeRate?from=${from}`).then((response) => {
+        response.json().then((data) => {
+            let exchange = document.getElementById(from);
+            exchange.innerHTML = `${from}&#8594;HUF ` + data["result"].toFixed(2);
+        });
+    });
+}
+
+loadNameDay();
+loadWeather();
+loadExchangeRate("EUR");
+loadExchangeRate("USD");
+
+const menuButton = document.getElementById("menu");
+menuButton.addEventListener("click", expandMenu());
+const sideBar = document.getElementById("sidebar");
+const collapseMenuButton = document.getElementById("collapse-menu");
+collapseMenuButton.addEventListener("click", collapseMenu());
+
+function expandMenu() {
+    sideBar.innerHTML = `
+    <div id="collapse-menu">X</div>
+    <div id="side-menu">
+        <div id="style">Sötét Mód</div>
+        <ul id="main-menu">
+            <p>Menü</p>
+            <li id="fresh">Legfrissebb</li>
+            <li id="most-viewed">Legnézettebb</li>
+        </ul>
+        <ul id="categories">
+            <p>Kategóriák</p>
+            <li id="domestic">Belföld</li>
+            <li id="foreign">Külföld</li>
+            <li id="sport">Sport</li>
+            <li id="culinary">Gasztró</li>
+            <li id="gaming">Gaming</li>
+            <li id="cinema">Mozi</li>
+            <li id="tv">TV</li>
+            <li id="techworld">Techvilág</li>
+            <li id="business">Tőzsde</li>
+        </ul>
+    </div>
+`
+}
+
+function collapseMenu() {
+    sideBar.innerHTML = "";
+}
