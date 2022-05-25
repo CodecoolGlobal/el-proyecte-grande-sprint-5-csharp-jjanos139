f15aa20b2777246bbfbf27ca7714e9b3ecdf4f20
jjanos139 <jdale139@gmail.com>
2022-05-25 11:18:25 +0200

    Add Domestic Controller and View

---
+++ Delightful Daily Dose/Controllers/DomesticController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Delightful_Daily_Dose.Controllers
+{
+    public class DomesticController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
