029cb843ad7a2bc445d72c7691020d168fce53f3
jjanos139 <jdale139@gmail.com>
2022-05-25 16:04:22 +0200

    Add Entertainment controller

---
+++ Delightful Daily Dose/Controllers/EntertainmentController.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Delightful_Daily_Dose.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Delightful_Daily_Dose.Controllers
+{
+    public class EntertainmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<string> GetEntertainmentNews()
+        {
+            string apiUrl =
+                "https://newsdata.io/api/1/news?apikey=pub_7623a07d5aec61d454d6ab40deb859282e19&language=hu&category=entertainment";
+            return await APIHelper.GetApi(apiUrl);
+        }
+    }
+}
