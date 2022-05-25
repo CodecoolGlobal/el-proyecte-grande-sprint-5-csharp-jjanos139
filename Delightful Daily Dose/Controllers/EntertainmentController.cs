242325e2da697ce0d520e08308eb41f4fd3f0d3d
jjanos139 <jdale139@gmail.com>
2022-05-25 16:19:50 +0200

    Update Entertainment controller and view

--- Delightful Daily Dose/Controllers/EntertainmentController.cs
+++ Delightful Daily Dose/Controllers/EntertainmentController.cs
@@ -1,21 +1,19 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Delightful_Daily_Dose.Helpers;
+using Delightful_Daily_Dose.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Delightful_Daily_Dose.Controllers
 {
     public class EntertainmentController : Controller
     {
-        public IActionResult Index()
+        private const string ApiUrl = "https://newsdata.io/api/1/news?apikey=pub_7623a07d5aec61d454d6ab40deb859282e19&language=hu&category=entertainment";
+
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
-        public async Task<string> GetEntertainmentNews()
-        {
-            string apiUrl =
-                "https://newsdata.io/api/1/news?apikey=pub_7623a07d5aec61d454d6ab40deb859282e19&language=hu&category=entertainment";
-            return await APIHelper.GetApi(apiUrl);
+            List<News> news = await APIHelper.GetNews(ApiUrl);
+            return View(news);
         }
     }
 }
