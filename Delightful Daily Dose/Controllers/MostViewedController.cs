d0d40a7fd3dfb01eedb1c925fdedeb90d5984dce
bosiTomi <bosanszkitamas@gmail.com>
2022-05-26 13:20:11 +0200

    Added new controller for most viewed news site

---
+++ Delightful Daily Dose/Controllers/MostViewedController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Delightful_Daily_Dose.Helpers;
+using Delightful_Daily_Dose.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Delightful_Daily_Dose.Controllers
+{
+    public class MostViewedController : Controller
+    {
+        private const string ApiUrl = "https://newsdata.io/api/1/news?apikey=pub_7623a07d5aec61d454d6ab40deb859282e19&language=hu&category=top";
+        public async Task<IActionResult> Index()
+        {
+            List<News> news = await APIHelper.GetNews(ApiUrl);
+            ViewData["Title"] = "Top";
+            return View("News", news);
+        }
+    }
+}
