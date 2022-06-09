3c0b1932d55f973cecf5bab4122e59f6fc018f04
bosiTomi <bosanszkitamas@gmail.com>
2022-06-09 11:07:14 +0200

    modified app to class instead of funtcion

--- delightful-daily-dose-frontend/src/App.js
+++ delightful-daily-dose-frontend/src/App.js
@@ -1,11 +1,19 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import { Component, useEffect } from "react";
+import { render } from "@testing-library/react";
 
-export default function App() {
-    return (
-        <>
-            <Header />
-            <Footer />
-        </>
-    )
+export default class App extends Component {
+    render() {
+        return (
+            <>
+                <Header />
+                <Footer />
+                <Sidebar />
+            </>
+                )
+    }
+        
+    
 }
