e9cf0d36ed9d9282da5811ddecfa9a3e485e2cb8
jjanos139 <jdale139@gmail.com>
2022-06-21 21:02:04 +0200

    Add Error page

---
+++ delightful-daily-dose-frontend/src/components/Error.js
@@ -0,0 +1,10 @@
+import ErrorImage from "../images/error.jpg"
+
+export default function Error() {
+    return (
+        <div>
+            <h6 style={{ textAlign: "center" }}>There's nothing to see here!</h6>
+            <img style={{ marginLeft: "450px", width: "600px" }} src={ErrorImage} />
+        </div>
+    )
+}
