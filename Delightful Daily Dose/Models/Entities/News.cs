eddfd57405a6650e1bb46cb56c9a90d48fde120a
galandras13 <galandras13@gmail.com>
2022-06-23 17:11:21 +0200

    Add DTOs for News and Category

--- Delightful Daily Dose/Models/Entities/News.cs
+++ Delightful Daily Dose/Models/Entities/News.cs
@@ -1,21 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Delightful_Daily_Dose.Models.Entities;
 
 namespace Delightful_Daily_Dose.Models;
 
 public class News
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long ID { get; set; }
     public string Title { get; set; }
     public string Link { get; set; }
-    public string SourceId { get; set; }
-    public List<string> Keywords { get; set; }
-    public List<string> Creator { get; set; }
     public string image_url { get; set; }
-    public string VideoUrl { get; set; }
     public string Description { get; set; }
     public string PubDate { get; set; }
     public string Content { get; set; }
-    public List<string> Country { get; set; }
-    public List<string> Category { get; set; }
+    public List<Category> Category { get; set; }
     public string Language { get; set; }
 
     public News(){}
