c519a53e4d01793365755a3383cc3d2c2d90220e
jjanos139 <jdale139@gmail.com>
2022-07-17 17:53:04 +0200

    Update Error page

--- delightful-daily-dose-frontend/src/components/Error.js
+++ delightful-daily-dose-frontend/src/components/Error.js
@@ -3,8 +3,7 @@ import ErrorImage from "../images/error.jpg"
 export default function Error() {
     return (
         <div>
-            <h6 style={{ textAlign: "center" }}>There's nothing to see here!</h6>
-            <img style={{ marginLeft: "450px", width: "600px" }} src={ErrorImage} alt="" />
+            <img src={ErrorImage} style={{ width: "50%", marginLeft: "25%", marginBottom: "50px", marginTop: "50px" }}></img>
         </div>
     )
 }
