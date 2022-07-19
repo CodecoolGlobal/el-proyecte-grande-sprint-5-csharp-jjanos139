d8750580f6417bbb61d707be8ae67c19b860fa1e
jjanos139 <jdale139@gmail.com>
2022-07-19 11:45:10 +0200

    Properties not yet in use

--- Delightful Daily Dose/Models/Entities/User.cs
+++ Delightful Daily Dose/Models/Entities/User.cs
@@ -9,7 +9,7 @@ namespace Delightful_Daily_Dose.Models.Entities
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public bool IsPublisher { get; set; }
-        public bool IsAdmin { get; set; }
+        //public bool IsPublisher { get; set; }
+        //public bool IsAdmin { get; set; }
     }
 }
