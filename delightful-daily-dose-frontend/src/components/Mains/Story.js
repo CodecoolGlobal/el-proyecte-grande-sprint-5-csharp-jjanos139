ef16aba6ac0727d200d3e8ce5a2c628020c4511f
jjanos139 <jdale139@gmail.com>
2022-07-21 17:13:41 +0200

    Add Story component

---
+++ delightful-daily-dose-frontend/src/components/Mains/Story.js
@@ -0,0 +1,37 @@
+import Card from 'react-bootstrap/Card';
+import React from "react";
+
+export default function Story(props) {
+    const [stories, setStories] = React.useState([]);
+    const dark = props.dark;
+
+    React.useEffect(() => {
+        fetch(`Stories`)
+            .then(response => response.json())
+            .then(data => setStories(data));
+    }, [])
+
+    return (
+        <div className="container">
+            <main role="main" className="pb-3">
+                <div id="body">
+                    {stories.map((item) => {
+                        const url = "Stories/" + item.id;
+                        const year = new Date(item.publishTime).getFullYear();
+                        const month = new Date(item.publishTime).getMonth() + 1;
+                        const day = new Date(item.publishTime).getDate();
+                        return (
+                            <Card key={item.id} className={dark === "dark" ? "dark" : ""}>
+                                {/* <img className="box-office-image" src={item.image} alt="" /> */}
+                                <div id="article-text">
+                                    <h5><a className={dark === "dark" ? "dark" : ""} href={url} target="_blank" rel="noreferrer">{item.title}</a></h5>
+                                    <h6 className={dark === "dark" ? "dark" : ""}>{item.content}</h6>
+                                    <p><i className="fa-solid fa-upload"></i> {year}-{month}-{day} </p>
+                                </div>
+                            </Card>)
+                    })}
+                </div>
+            </main >
+        </div>
+    )
+}
