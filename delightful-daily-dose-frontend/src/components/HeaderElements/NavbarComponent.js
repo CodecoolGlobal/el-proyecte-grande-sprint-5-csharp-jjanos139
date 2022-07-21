44836ca199cfad4a8412831a58f736686c9483a7
jjanos139 <jdale139@gmail.com>
2022-07-21 15:41:16 +0200

    Update login button function

--- delightful-daily-dose-frontend/src/components/HeaderElements/NavbarComponent.js
+++ delightful-daily-dose-frontend/src/components/HeaderElements/NavbarComponent.js
@@ -4,15 +4,25 @@ import WeatherAndExchange from "./WeatherAndExchange";
 import Menu from "../Menu"
 import { Link } from 'react-router-dom';
 import Cookies from 'universal-cookie';
+import React from "react";
 
 export default function Navbar(props) {
     const cookies = new Cookies();
-    const user = cookies.get('user')
+    const [user, setUser] = React.useState(cookies.get("user"))
     function logout() {
-        fetch("/Logout", {
-            method: "POST"
-        }).then(window.location.reload())
+        fetch("/Logout",
+            {
+                method: "POST"
+            }).then(window.location.reload());
     }
+    
+    function googleLogin() {
+        fetch("/googleLogin",
+            {
+                method: 'POST',
+            }).then(response => console.log(response));
+    }
+
     return (
         <div className="container">
             {props.elements === "top" ? <DateAndName /> : ""}
@@ -20,10 +30,11 @@ export default function Navbar(props) {
             {props.elements === "top" ? <WeatherAndExchange /> : ""}
             {props.elements === "lower" ? <Menu /> : ""}
             {props.elements === "lower" ? <span id="sitename" className="navbar-brand"><img width="150" alt="" /></span> : ""}
-            {props.elements === "lower" && !user ? <Link to="/Register" className="reg-topright">Register</Link> : ""}
-            {props.elements === "lower" && !user ? <Link to="/Login" className="login-topright">Login</Link> : ""}
-            {props.elements === "lower" && user ? <h5 className="reg-topright">{user}</h5> : ""}
-            {props.elements === "lower" && user ? <Link to="/" onClick={logout} className="login-topright">Logout</Link> : ""}
+            {props.elements === "lower" && typeof user === "undefined" ? <Link to="/Register" className="reg-topright">Register</Link> : ""}
+            {props.elements === "lower" && typeof user === "undefined" ? <Link to="/Login" className="login-topright">Login</Link> : ""}
+            {props.elements === "lower" && typeof user === "undefined" ? <h5 onClick={googleLogin}><i className="fa-brands fa-google"></i></h5> : ""}
+            {props.elements === "lower" && typeof user === "string" ? <h5 className="reg-topright">{user}</h5> : ""}
+            {props.elements === "lower" && typeof user === "string" ? <Link to="/" onClick={logout} className="login-topright">Logout</Link> : ""}
         </div>
     )
 }
--- delightful-daily-dose-frontend/src/components/User/Login.js
+++ delightful-daily-dose-frontend/src/components/User/Login.js
@@ -1,6 +1,28 @@
+import React from "react";
+import { useNavigate } from "react-router-dom"
+
 export default function Login(props) {
+    const [username, setUserName] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            Username: username,
+            Password: password
+        })
+    };
+    const nav = useNavigate()
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        fetch("/Login", requestOptions)
+            .then(() => props.setUser(username))
+            .then(nav("/"), { state: { username: username } })
+    };
+
     return (
-        <form method="post">
+        <form method="post" onSubmit={handleSubmit}>
             <h3 className="login-h3">Login Here</h3>
             <label htmlFor="username">Username</label>
             <input
@@ -11,7 +33,8 @@ export default function Login(props) {
                 autoComplete="username"
                 required="Required"
                 pattern="^[a-zA-Z0-9_\\.-]{3,20}"
-
+                value={username}
+                onChange={event => setUserName(event.target.value)}
             />
             <label htmlFor="password">Password</label>
             <input
@@ -22,9 +45,10 @@ export default function Login(props) {
                 autoComplete="new-password"
                 required="Required"
                 pattern="^([a-zA-Z0-9@*#]{8,15})$"
+                value={password}
+                onChange={event => setPassword(event.target.value)}
             />
             <button className="login-button">Login</button>
         </form>
     )
-
 }
