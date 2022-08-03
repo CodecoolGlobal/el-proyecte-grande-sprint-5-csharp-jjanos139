8341aaf2d16a99a03666e9d2d1b212cd12faf7ab
bosiTomi <bosanszkitamas@gmail.com>
2022-08-03 15:38:16 +0200

    more tests

--- Delightful Daily Dose Test/UnitTest1.cs
+++ Delightful Daily Dose Test/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using Microsoft.Extensions.Options;
 
 namespace Delightful_Daily_Dose_Test
@@ -14,7 +15,7 @@ namespace Delightful_Daily_Dose_Test
         }
 
         [Test]
-        public void Test1()
+        public void EmailIsUniqueHappyPath()
         {
             UserRepository userRepository = new UserRepository(_context, httpContextAccessor);
             var email = "foispanne@varmegyehaza.hu";
@@ -23,11 +24,81 @@ namespace Delightful_Daily_Dose_Test
         }
 
         [Test]
-        public void Test2()
+        public void EmailIsUniqueSadPath()
         {
             UserRepository userRepository = new UserRepository(_context, httpContextAccessor);
             var email2 = "ddd@bitemyshinymetalass.com";
             Assert.IsFalse(userRepository.IsEmailUniq(email2));
+        }
+
+        [Test]
+        public void UsernameUniqueHappyTest()
+        {
+            UserRepository userRepository = new UserRepository(_context, httpContextAccessor);
+            var username = "Admin";
+            Assert.IsTrue(userRepository.IsUsernameUniq(username));
+        }
+
+        [Test]
+        public void UsernameUniqueSadTest()
+        {
+            UserRepository userRepository = new UserRepository(_context, httpContextAccessor);
+            var username = "admin";
+            Assert.IsFalse(userRepository.IsUsernameUniq(username));
+        }
+
+        [Test]
+        public void GetAllUsersTestHappyPath()
+        {
+            UserRepository userRepository = new UserRepository(_context, httpContextAccessor);
+            Assert.AreEqual(_context.User, userRepository.GetAllUsers());
+        }
+
+
+        [Test]
+        public async Task TestAuthenticateHappyPath()
+        {
+            var newUser = new User
+            {
+                Id = "6464648",
+                Username = "admin",
+                Email = "ddd@bitemyshinymetalass.com",
+                Password = "admin111",
+                Role = "Admin"
+            };
+            
+            UserRepository userRepository = new UserRepository(_context, httpContextAccessor);
+            var username = "admin";
+            var password = "admin111";
+            var actual = await userRepository.AuthenticateAsync(username, password);
+            Assert.AreEqual("6464648", actual.Id);
+            Assert.AreEqual("admin", actual.Username);
+            Assert.AreEqual("ddd@bitemyshinymetalass.com", actual.Email);
+            Assert.AreEqual("admin111", actual.Password);
+            Assert.AreEqual("Admin", actual.Role);
+        }
+
+        [Test]
+        public async Task TestAuthenticateSadPath()
+        {
+            var newUser = new User
+            {
+                Id = "6464648",
+                Username = "admin",
+                Email = "ddd@bitemyshinymetalass.com",
+                Password = "admin111",
+                Role = "Admin"
+            };
+
+            UserRepository userRepository = new UserRepository(_context, httpContextAccessor);
+            var username = "admin";
+            var password = "admin111";
+            var actual = await userRepository.AuthenticateAsync(username, password);
+            Assert.AreNotEqual("646468", actual.Id);
+            Assert.AreNotEqual("adin", actual.Username);
+            Assert.AreNotEqual("dd@bitemyshinymetalass.com", actual.Email);
+            Assert.AreNotEqual("adin111", actual.Password);
+            Assert.AreNotEqual("Adn", actual.Role);
         }
     }
 }
